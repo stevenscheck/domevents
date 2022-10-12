a86da3725157e6831c41db8452d306e2b6adf8c2
Jakob Curtis <jakobccurtis@gmailcom>
2022-10-12 17:11:45 -0400

    magnification

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ button.addEventListener('click', function (){
     count.textContent = Number(count.textContent) +1
 })
 
+let p = document.querySelector("#placeholder")
+
+const button2 = document.querySelector("#grow")
+grow.addEventListener("click", function(){
+    
+    style = window.getComputedStyle(p, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    p.style.fontSize = (currentSize + 5)+"px"    
+
+})
+
+const button3 = document.querySelector("#shrink")
+shrink.addEventListener("click", function(){
+    
+    style = window.getComputedStyle(p, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    p.style.fontSize = (currentSize - 5)+"px"    
+
+})
+
